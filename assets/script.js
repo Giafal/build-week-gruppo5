19d2cf7ab06290d579571aa69248880ee062e659
Bjoorng <luca.sernicola@gmail.com>
2023-04-17 17:52:29 +0200

    modified js

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 
 let h2 = document.createElement('h2');
 h2.innerHTML = 'Welcome to <b>your exam</b>';
-let title = document.querySelector('#title');
+let title = document.querySelector('.title');
 title.appendChild(h2);
 
 let h3 = document.createElement('h3');
 let p = document.createElement('p');
 p.innerHTML = "We don't expect most engineers to know the answers to all of these questions, so don't worry if you're unsure of a few of them.";
 h3.innerHTML = 'Instructions';
-let subtitle = document.querySelector('#subtitle');
+let subtitle = document.querySelector('.subtitle');
 subtitle.appendChild(h3);
 subtitle.appendChild(p);
 
@@ -18,7 +18,7 @@ subtitle.appendChild(p);
 
     arr = ['each question is timed and can onyl be answered once', 'changing browser tab or opening other windows will invalidate the question', 'this exam will take approx. 0-5 minutes.']
 
-    document.querySelector('#myList').appendChild(ul);
+    document.querySelector('.myList').appendChild(ul);
     arr.forEach(printList);
     function printList (element, index, arr) {
         let li = document.createElement('li');
