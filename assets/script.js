6b99cb7b1208b35344498933f08050b69f32c4d9
Andreaisgro01 <andrea.isgro01@gmail.com>
2023-04-18 12:52:44 +0200

    br2

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,13 @@
 
 {
 let h2 = document.createElement('h2');
-h2.innerHTML = 'Welcome to <br>your exam</br>';
+h2.innerHTML = 'Welcome to <br>your exam';
 let title = document.querySelector('.title');
 title.appendChild(h2);
 
 let h3 = document.createElement('h3');
 let p = document.createElement('p');
-p.innerHTML = "We don't expect most engineers to know the answers to all of these questions, so don't worry if you're unsure of a few of them.";
+p.innerHTML = "We don't expect most engineers to know the answers to all of these <br>questions, so don't worry if you're unsure of a few of them.";
 h3.innerHTML = 'Instructions';
 let subtitle = document.querySelector('.subtitle');
 subtitle.appendChild(h3);
