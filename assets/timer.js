b53cabbd6868ad6e92d32953066ee2f5fb7db78d
Bjoorng <luca.sernicola@gmail.com>
2023-04-19 12:32:56 +0200

    clock

--- assets/timer.js
+++ assets/timer.js
@@ -1,14 +1,22 @@
 let timer = document.querySelector('.timer');
+let upSec = document.createElement('p');
 let clock = document.createElement('div');
+let remain = document.createElement('p');
+upSec.setAttribute('class', 'upSec');
 clock.setAttribute('class', 'clock');
-timer.appendChild(clock);
+remain.setAttribute('class','remain');
+upSec.textContent = 'SECONDS';
+remain.textContent = 'remaining';
+timer.append(upSec);
+timer.append(clock);
+timer.append(remain);
 
 let seconds = 10;
 let countdown = setInterval(function() {
-    if (seconds == 0){
+    if (seconds < 0){
         clearInterval(countdown);
     } else{
-        document.querySelector('.clock').innerHTML = seconds;
+        document.querySelector('.clock').innerHTML =  seconds;
     }
     seconds -= 1;
 
